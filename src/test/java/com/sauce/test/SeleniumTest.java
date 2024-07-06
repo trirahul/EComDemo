03b3581a92aeedd39137c6ab1f163703db85c18d
Rahul <trirahul85@gmail.com>
2024-07-07 02:19:11 +0530

    Add Maven project files

--- src/test/java/com/sauce/test/SeleniumTest.java
+++ src/test/java/com/sauce/test/SeleniumTest.java
@@ -1,5 +1,6 @@
-package com.gmail.test;
+package com.sauce.test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -8,6 +9,8 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.sauce.utils.HelperFunctions;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumTest {
@@ -15,25 +18,34 @@ public class SeleniumTest {
 
     @BeforeClass
     public void setUp() {
-        // Set the path to the ChromeDriver executable
-//        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
     	WebDriverManager.chromedriver().browserVersion("126.0.6478.127").setup();
 //        WebDriverManager.chromedriver().setup();
         // Create an instance of ChromeOptions
+    	
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-sync");
+//        options.addArguments("--headless");
         // Initialize a new WebDriver instance of ChromeDriver with options
         driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
     }
 
     @Test
-    public void testGoogleTitle() {
+    public void testGoogleTitle() throws Exception {
         // Open a website
+	HelperFunctions help = null;
         driver.get("https://www.google.com");
-
+        help = new HelperFunctions(driver);
         // Get the title of the page
         String title = driver.getTitle();
-
+        
+        help.customExplicitWaitTillElementBecomesActive("//textarea[@aria-label='Search']", 2, 1);
+        help.customExplicitWaitTillElementBecomesClickable("//textarea[@aria-label='Search']", 5, 0.5);
+        driver.findElement(By.xpath("//textarea[@aria-label='Search']")).click();
+        help.customExplicitWaitTillElementBecomesClickable("//textarea[@aria-label='Search']", 5, 0.5);
+        driver.findElement(By.xpath("//textarea[@aria-label='Search']")).sendKeys("Hello Bhai");
         // Print the title of the page
         System.out.println("Title: " + title);
 
